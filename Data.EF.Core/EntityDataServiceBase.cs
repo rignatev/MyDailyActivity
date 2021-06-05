8feb9e66f5f3c1ace91f19a2aa508905bc114369
Roman Ignatev <rignatev@gmail.com>
2021-06-05 21:51:14 +0300

    Dependencies update.

--- Data.EF.Core/EntityDataServiceBase.cs
+++ Data.EF.Core/EntityDataServiceBase.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 using Data.Contracts.EntityDataServices;
 using Data.Contracts.EntityOrm;
